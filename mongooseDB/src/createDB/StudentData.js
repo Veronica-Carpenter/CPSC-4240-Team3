750b8a8af4f77fa1e5e309c3f078adc7b8504f2b
Veronica-Carpenter <veronica1north@gmail.com>
2022-05-04 16:34:29 -0700

    Added additional students to StudentData.js

--- mongooseDB/src/createDB/StudentData.js
+++ mongooseDB/src/createDB/StudentData.js
@@ -3,9 +3,10 @@ db = db.getSiblingDB('attendance-tracker')
 db.createCollection('students')
 studentsCollection = db.getCollection("students")
 studentsCollection.remove({})
+
+// Sarah's courses and attendance
 studentsCollection.insert(
 {
-    // classes Sarah is taking
     studentId : 1,
     fname: "Sarah",
     lname: "Smith",
@@ -86,7 +87,7 @@ studentsCollection.insert(
        ] 
 }
 )
-// classes Ash is taking
+// Ash's courses and attendance
 studentsCollection.insert(
     {
         studentId : 2,
@@ -169,4 +170,243 @@ studentsCollection.insert(
            ] 
     }
 )
+ 
+// John's courses and attendance
+studentsCollection.insert(
+    {
+        studentId : 3,
+        fname: "John",
+        lname: "Doe",
+        email: "john.doe@edu.edu",
+        courseList : [
+         {
+            courseId: 1,
+            courseName: "CPSC 4310: Machine Learning"
+         },
+         {
+            courseId: 3,
+            courseName: "CPSC 4220: User Experience Design"
+         }
+        ],
+        attendanceList : [
+            // ML lectures
+            {
+                lectureId: 1,
+                date: "04-05-2022",
+                studentId: 3,
+                status: "Present"
+            },
+            {
+                lectureId: 2,
+                date: "04-07-2022",
+                studentId: 3,
+                status: "Present"
+            },
+            {
+                lectureId: 3,
+                date: "04-12-2022",
+                studentId: 3,
+                status: "Present"
+            },
+            {
+                lectureId: 4,
+                date: "04-14-2022",
+                studentId: 3,
+                status: "Present"
+            },
+            {
+                lectureId: 5,
+                date: "04-19-2022",
+                studentId: 3,
+                status: "Late"
+            },
+            // UX lectures
+            {
+                lectureId: 111,
+                date: "04-04-2022",
+                studentId: 3,
+                status: "Present"
+            },
+            {
+                lectureId: 222,
+                date: "04-06-2022",
+                studentId: 3,
+                status: "Present"
+            },
+            {
+                lectureId: 333,
+                date: "04-11-2022",
+                studentId: 3,
+                status: "Present"
+            },
+            {
+                lectureId: 444,
+                date: "04-13-2022",
+                studentId: 3,
+                status: "Present"
+            },
+            {
+                lectureId: 555,
+                date: "04-18-2022",
+                studentId: 3,
+                status: "Present"
+            }
+           ] 
+    }
+)
+  
+// Jane's courses and attendance
+studentsCollection.insert(
+    {
+        studentId : 4,
+        fname: "Jane",
+        lname: "Doe",
+        email: "jane.doe@edu.edu",
+        courseList : [
+         {
+            courseId: 1,
+            courseName: "CPSC 4310: Machine Learning"
+         }
+        ],
+        attendanceList : [
+            // ML lectures
+            {
+                lectureId: 1,
+                date: "04-05-2022",
+                studentId: 4,
+                status: "Present"
+            },
+            {
+                lectureId: 2,
+                date: "04-07-2022",
+                studentId: 4,
+                status: "Absent"
+            },
+            {
+                lectureId: 3,
+                date: "04-12-2022",
+                studentId: 4,
+                status: "Present"
+            },
+            {
+                lectureId: 4,
+                date: "04-14-2022",
+                studentId: 4,
+                status: "Present"
+            },
+            {
+                lectureId: 5,
+                date: "04-19-2022",
+                studentId: 4,
+                status: "Late"
+            }
+           ] 
+    }
+)
+
+// Alexander's courses and attendance
+studentsCollection.insert(
+    {
+        studentId : 5,
+        fname: "Alexander",
+        lname: "Forbes",
+        email: "alexander.forbes@edu.edu",
+        courseList : [
+         {
+            courseId: 3,
+            courseName: "CPSC 4220: User Experience Design"
+         }
+        ],
+        attendanceList : [
+            // UX lectures
+            {
+                lectureId: 111,
+                date: "04-04-2022",
+                studentId: 5,
+                status: "Present"
+            },
+            {
+                lectureId: 222,
+                date: "04-06-2022",
+                studentId: 5,
+                status: "Present"
+            },
+            {
+                lectureId: 333,
+                date: "04-11-2022",
+                studentId: 5,
+                status: "Present"
+            },
+            {
+                lectureId: 444,
+                date: "04-13-2022",
+                studentId: 5,
+                status: "Present"
+            },
+            {
+                lectureId: 555,
+                date: "04-18-2022",
+                studentId: 5,
+                status: "Present"
+            }
+           ] 
+    }
+)
     
+// Crystal's courses and attendance
+studentsCollection.insert(
+    {
+        studentId : 6,
+        fname: "Crystal",
+        lname: "Matthews",
+        email: "crystal.matthews@edu.edu",
+        courseList : [
+         {
+            courseId: 1,
+            courseName: "CPSC 4310: Machine Learning"
+         },
+         {
+            courseId: 2,
+            courseName: "CPSC 4240: Software as a Service"
+         },
+         {
+            courseId: 3,
+            courseName: "CPSC 4220: User Experience Design"
+         }
+        ],
+        attendanceList : [
+            // Saas lectures
+            {
+                lectureId: 11,
+                date: "04-04-2022",
+                studentId: 6,
+                status: "Present"
+            },
+            {
+                lectureId: 22,
+                date: "04-06-2022",
+                studentId: 6,
+                status: "Present"
+            },
+            {
+                lectureId: 33,
+                date: "04-11-2022",
+                studentId: 6,
+                status: "Present"
+            },
+            {
+                lectureId: 44,
+                date: "04-13-2022",
+                studentId: 6,
+                status: "Absent"
+            },
+            {
+                lectureId: 55,
+                date: "04-18-2022",
+                studentId: 6,
+                status: "Present"
+            }
+           ] 
+    }
+)
+        
