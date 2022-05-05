53f7b753ff9f4e6c9ffac40478d8ef835e304b97
Veronica-Carpenter <veronica1north@gmail.com>
2022-05-04 17:09:06 -0700

    Added additional courses to AttendanceData.js

--- mongooseDB/src/createDB/AttendanceData.js
+++ mongooseDB/src/createDB/AttendanceData.js
@@ -2,23 +2,41 @@ db = db.getSiblingDB('attendance-tracker')
 
 // courses data
 db.createCollection('courses')
-allCoursesCollection = db.getCollection("courses")
-allCoursesCollection.remove({})
-allCoursesCollection.insert(
+coursesCollection = db.getCollection("courses")
+coursesCollection.remove({})
+coursesCollection.insert(
     {
         courseId: 1,
         courseName: "CPSC 4310: Machine Learning"
     }
 )
-allCoursesCollection.insert(
+coursesCollection.insert(
     {
         courseId: 2,
         courseName: "CPSC 4240: Software as a Service"
     }
 )
-allCoursesCollection.insert(
+coursesCollection.insert(
     {
         courseId: 3,
         courseName: "CPSC 4220: User Experience Design"
+    }
+)
+coursesCollection.insert(
+    {
+        courseId: 4,
+        courseName: "CPSC 4260: Refactoring & Software Design"
+    }
+)
+coursesCollection.insert(
+    {
+        courseId: 5,
+        courseName: "CPSC 4300: Phys Database Design and Opt"
+    }
+)
+coursesCollection.insert(
+    {
+        courseId: 6,
+        courseName: "CPSC 4510 Computer Networks"
     }
 )
