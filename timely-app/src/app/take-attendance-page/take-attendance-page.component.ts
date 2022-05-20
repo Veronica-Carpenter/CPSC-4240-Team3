36bdfcceeaa515a9b18893634c053c3c2138528c
phuongtang1201 <mtang1@seattleu.edu>
2022-05-20 15:39:05 -0700

    Added two input studentID and sessionCode for take-attendance-component

--- timely-app/src/app/take-attendance-page/take-attendance-page.component.ts
+++ timely-app/src/app/take-attendance-page/take-attendance-page.component.ts
@@ -7,9 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TakeAttendancePageComponent implements OnInit {
 
+  studentID: number = 0;
+  code: number = 0;
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  getStudentId(val: string){
+    //check if student id is number
+    if(!isNaN(Number(val))){
+      this.studentID = parseInt(val);
+      console.log("Student ID: " + this.studentID);
+    }
+    else(
+      console.log("Student ID need to be number")
+    )
+  }
+
+  getCode(val: string){
+    //check if code is number
+    if(!isNaN(Number(val))){
+      this.code = parseInt(val);
+      console.log("Session code: " + this.code);
+    }
+    else(
+      console.log("Session code need to be number")
+    )
+  }
+
+  submitButton(){
+    console.log("student id: " + this.studentID + "; session code: "+this.code);
+  }
+
+  
 
 }
