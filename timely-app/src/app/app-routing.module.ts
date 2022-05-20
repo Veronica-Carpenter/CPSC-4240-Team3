b6e9a3026dc620e44448e3ac9dc595012dc9df59
phuongtang1201 <mtang1@seattleu.edu>
2022-05-20 14:37:30 -0700

    add route for take-attendance in app-rounting.module.ts

--- timely-app/src/app/app-routing.module.ts
+++ timely-app/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TakeAttendancePageComponent } from './take-attendance-page/take-attendance-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'takeAttendance', component: TakeAttendancePageComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
