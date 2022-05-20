21b1a5c2c49832690591b506138acd58f09e9bfd
shipra <fshipra@seattleu.edu>
2022-05-20 15:51:38 -0700

    Added tests for express routes

--- mongooseDB/test/test.js
+++ mongooseDB/test/test.js
@@ -1,6 +1,6 @@
 var chai = require('chai');
 var chaiHttp = require('chai-http');
-var app = require('../src/App')
+var app = require('../src/App');
 
 var assert = chai.assert;
 var expect = chai.expect;
@@ -9,13 +9,140 @@ var should = chai.should();
 var http = require('http');
 chai.use(chaiHttp);
 
-describe('sample test', () => {
-    it('should get all the students', (done) => {
-        assert.equal(-1, [1,2,3].indexOf(5));
-        assert.equal(-1, [1,2,3].indexOf(0));
+describe('Testing API routes', () => {
+
+    // Test the GET student routes
+    describe("Get all students", () => {
+
+        var requestResult;
+        var response; 
+
+        before((done) => {
+            chai.request("http://localhost:8080")
+            .get("/students")
+			.end((err, res) => {
+				requestResult = res.body;
+				response = res;
+                expect(err).to.be.null;
+                expect(response).to.have.status(200);
+				done();
+			});
+        });
+
+        it('should return array of object with more than 1 object', () => {
+            response.should.have.status(200);
+            response.body.should.be.an('array');
+            response.body.length.should.be.eq(7);
+            response.body.should.not.be.a('string');
+        });
+
+        it('should have first entry of array with some known properties', () => {
+            expect(requestResult[0]).to.include.keys('studentId');
+            expect(requestResult[0]).to.include.keys('fname');
+            expect(requestResult[0]).to.include.keys('lname');
+            expect(requestResult[0]).to.include.keys('email');
+            expect(requestResult[0]).to.have.deep.property('courseList').that.is.a('array');
+            expect(requestResult[0]).to.have.deep.property('attendanceList').that.is.a('array');
+        });
+    })
+
+    // Test the GET professor routes
+    describe("Get all professors", () => {
+
+        var requestResult;
+        var response; 
+
+        before((done) => {
+            chai.request("http://localhost:8080")
+            .get("/professors")
+			.end((err, res) => {
+				requestResult = res.body;
+				response = res;
+                expect(err).to.be.null;
+                expect(response).to.have.status(200);
+				done();
+			});
+        });
+
+        it('should return array of object with more than 1 object', () => {
+            response.should.have.status(200);
+            response.body.should.be.an('array');
+            response.body.length.should.be.eq(4);
+            response.body.should.not.be.a('string');
+        });
+
+        it('should have first entry of array with some known properties', () => {
+            expect(requestResult[0]).to.include.keys('professorId');
+            expect(requestResult[0]).to.include.keys('fname');
+            expect(requestResult[0]).to.include.keys('lname');
+            expect(requestResult[0]).to.include.keys('email');
+            expect(requestResult[0]).to.have.deep.property('courseList').that.is.a('array');
+        });
     });
 
-    it('new World!', function(){
-        
+    // Test the GET courses routes
+    describe("Get all courses", () => {
+
+        var requestResult;
+        var response; 
+
+        before((done) => {
+            chai.request("http://localhost:8080")
+            .get("/courses")
+			.end((err, res) => {
+				requestResult = res.body;
+				response = res;
+                expect(err).to.be.null;
+                expect(response).to.have.status(200);
+				done();
+			});
+        });
+
+        it('should return array of object with more than 1 object', () => {
+            response.should.have.status(200);
+            response.body.should.be.an('array');
+            response.body.length.should.be.eq(7);
+            response.body.should.not.be.a('string');
+        });
+
+        it('should have first entry of array with some known properties', () => {
+            expect(requestResult[0]).to.include.keys('courseId');
+            expect(requestResult[0]).to.include.keys('courseName');
+            expect(requestResult[0]).to.include.keys('_id');
+        });
     });
-});
+
+    // Test the GET lectures routes
+    describe("Get all courses", () => {
+
+        var requestResult;
+        var response; 
+
+        before((done) => {
+            chai.request("http://localhost:8080")
+            .get("/lectures")
+			.end((err, res) => {
+				requestResult = res.body;
+				response = res;
+                expect(err).to.be.null;
+                expect(response).to.have.status(200);
+				done();
+			});
+        });
+
+        it('should return array of object with more than 1 object', () => {
+            response.should.have.status(200);
+            response.body.should.be.an('array');
+            response.body.length.should.be.eq(16);
+            response.body.should.not.be.a('string');
+        });
+
+        it('should have first entry of array with some known properties', () => {
+            expect(requestResult[0]).to.include.keys('courseId');
+            expect(requestResult[0]).to.include.keys('lectureId');
+            expect(requestResult[0]).to.include.keys('courseName');
+            expect(requestResult[0]).to.include.keys('date');
+            expect(requestResult[0]).to.include.keys('secureCode');
+        });
+    });
+})
