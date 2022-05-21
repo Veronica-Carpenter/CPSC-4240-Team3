68139651aa5b488f80a374c4f122218ebbfe4653
shipra <fshipra@seattleu.edu>
2022-05-21 15:58:08 -0700

    Fixed the path for professor component

--- timely-app/src/app/app-routing.module.ts
+++ timely-app/src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TakeAttendancePageComponent } from './take-attendance-page/take-attendance-page.component';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { ProfessorHomePageComponent } from './professor-home-page/professor-home-page.component';
 
 const routes: Routes = [
   { path: 'welcomePage', component: WelcomePageComponent },
   { path: '', redirectTo: '/welcomePage', pathMatch: 'full' },
-  { path: 'ProfessorHomePageComponent', component: TakeAttendancePageComponent},
+  { path: 'ProfessorHomePageComponent', component: ProfessorHomePageComponent},
   { path: 'takeAttendance', component: TakeAttendancePageComponent},
   
 ];
