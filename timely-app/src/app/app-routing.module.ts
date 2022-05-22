634313ebdc793bd5f9dbddf3472a0fbfd5370b40
Asra Fatima <afatima@seattleu.edu>
2022-05-21 21:32:11 -0700

    Added additional routes for update components

--- timely-app/src/app/app-routing.module.ts
+++ timely-app/src/app/app-routing.module.ts
@@ -3,12 +3,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { TakeAttendancePageComponent } from './take-attendance-page/take-attendance-page.component';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 import { ProfessorHomePageComponent } from './professor-home-page/professor-home-page.component';
+import { AddStudentsComponent} from './add-students/add-students.component';
+import { DeleteStudentsComponent} from './delete-students/delete-students.component';
+import { DeletionConfirmComponent} from './deletion-confirm/deletion-confirm.component';
 
 const routes: Routes = [
   { path: 'welcomePage', component: WelcomePageComponent },
   { path: '', redirectTo: '/welcomePage', pathMatch: 'full' },
   { path: 'ProfessorHomePageComponent', component: ProfessorHomePageComponent},
   { path: 'takeAttendance', component: TakeAttendancePageComponent},
+  { path: 'AddStudents', component: AddStudentsComponent},
+  { path: 'DeleteStudents', component: DeleteStudentsComponent},
+  { path: 'DeletionConfirm', component: DeletionConfirmComponent}
   
 ];
 
