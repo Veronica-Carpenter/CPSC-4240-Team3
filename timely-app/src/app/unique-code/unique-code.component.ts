b0e10bcca461f20ceb0d6e61ae377457840daf09
Asra Fatima <afatima@seattleu.edu>
2022-05-22 20:49:54 -0700

    Unique Code for students

---
+++ timely-app/src/app/unique-code/unique-code.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-unique-code',
+  templateUrl: './unique-code.component.html',
+  styleUrls: ['./unique-code.component.css']
+})
+export class UniqueCodeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
