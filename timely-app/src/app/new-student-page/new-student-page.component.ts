d2feda5762fcd41d7bd06e302c18ce47c150344b
shipra <fshipra@seattleu.edu>
2022-05-25 00:40:21 -0700

    Created the UI for new student page

--- timely-app/src/app/new-student-page/new-student-page.component.ts
+++ timely-app/src/app/new-student-page/new-student-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { StudentClass } from '../student-class';
+import { TimelyAPIService } from '../timely-api.service';
 
 @Component({
   selector: 'app-new-student-page',
@@ -7,9 +9,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewStudentPageComponent implements OnInit {
 
-  constructor() { }
+  studentID: number = 0;
+  studentResult: StudentClass;
+
+  formDisplay = true;
+  successfulMessageDisplay = false;
+  invalidStudentIdMessageDisplay = false;
+  deniedSubmissionDisplay = false;
+
+  constructor(private apiService: TimelyAPIService) { 
+    
+  }
 
   ngOnInit(): void {
   }
 
+  getStudentId(val: string){
+
+    //check if student id is number
+    if(!isNaN(Number(val))){
+      this.studentID = parseInt(val);
+    }
+    else(
+      console.log("Student ID should be a number")
+    )
+  }
+
+  reloadPage(){
+    window.location.reload();
+  }
+
+  submitButton(){ 
+    let studentObjId :any ; 
+
+    //Turn off all error message
+
+    this.invalidStudentIdMessageDisplay = false;
+    this.apiService.getStudentByStudentId(this.studentID).subscribe((result: any)=>{
+      this.studentResult = result; 
+      if (result == null) {
+        this.invalidStudentIdMessageDisplay = true;
+      }
+    });
+  }
 }
