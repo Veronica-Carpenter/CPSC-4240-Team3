b1f10bfa8f1a4c7152cdd68674c7a223ff0fb3e6
phuongtang1201 <mtang1@seattleu.edu>
2022-05-26 15:10:56 -0700

    Implemented lecture-list

--- timely-app/src/app/lecture-list-page/lecture-list-page.component.ts
+++ timely-app/src/app/lecture-list-page/lecture-list-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { TimelyAPIService } from '../timely-api.service';
+import { ActivatedRoute } from '@angular/router';
+import { LectureClass } from '../lecture-class';
 @Component({
   selector: 'app-lecture-list-page',
   templateUrl: './lecture-list-page.component.html',
@@ -7,9 +9,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LectureListPageComponent implements OnInit {
 
-  constructor() { }
+  courseId: number = 0;
+  lectureLists: LectureClass[];
+  lectureDate: string;
+  courseName: string;
+
+  constructor(private apiService: TimelyAPIService, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.activatedRoute.params.forEach((route) => this.courseId = route["courseId"]);
+    console.log("courseID: " + this.courseId);
+    this.apiService.getLecturesByCourse(this.courseId).subscribe((result: any)=>{
+      this.lectureLists = result;
+      console.log("Lecture List: " + JSON.stringify(this.lectureLists));
+      
+      if(this.lectureLists.length>0){
+        this.courseName = this.lectureLists[0].courseName;
+      }
+      
+    });
+  }
+
+  formatDate (date: string){
+    let tempDate = new Date(date);
+      return tempDate.toDateString();
+  }
+
+  generateCodeClick(){
+    //The today date 
+    let todayDate = new Date();
+
+    let lectureObject={
+      "courseId" :this.courseId,
+      "date": todayDate
+    }
+    
+    //Call POST API to add new lecture 
+    this.apiService.addLecture(lectureObject).subscribe((result:any)=>{
+      console.log("Lecture Result: "+ JSON.stringify(result));
+      this.reloadPage();
+    });
+
+  }
+
+  reloadPage(){
+    window.location.reload();
   }
 
 }
